2a9031134541a4e59f8eff0a5f10efc1288036a7
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2020-11-02 07:37:12 -0500

    eye detecting modified

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Eye-tracking
+#
+#
+# Starting from article medium.com: https://medium.com/@stepanfilonov/tracking-your-eyes-with-python-3952e66194a6
+
+
--- eye detecting.py
+++ eye detecting.py
@@ -1,27 +1,39 @@
 import cv2
 import numpy as np
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    img = cv2.imread(“eye image.jpg”)
+ # Step 1: Open the image
+ 
+img = cv2.imread('eye_image.jpg');
 
-    cv2.imshow('my image',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow('my image',img)
+cv2.waitKey(0)
 
-    def detect_eyes(img, classifier):
-        gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        eyes = cascade.detectMultiScale(gray_frame, 1.3, 5) # detect eyes
-        width = np.size(image, 1) # get face frame width
-        height = np.size(image, 0) # get face frame height
+ # Step 2: detect the eye
 
-        for (x, y, w, h) in eyes:
-            eyecenter = w/2
-            eye_frame = img[y:y + h, x:x + w]
+ # (some statements here)
+
+ # Step 3: show the location of the detected eye
+
+ # Step 4: clean up and quit
+
+cv2.destroyAllWindows()
+ 
+
+def detect_eyes(img, classifier):
+	gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	eyes = cascade.detectMultiScale(gray_frame, 1.3, 5) # detect eyes
+	width = np.size(image, 1) # get face frame width
+	height = np.size(image, 0) # get face frame height
+
+	for (x, y, w, h) in eyes:
+		eyecenter = w/2
+		eye_frame = img[y:y + h, x:x + w]
 
 
-    detector_params = cv2.SimpleBlobDetector_Params()
-    detector_params.filterByArea = True
-    detector_params.maxArea = 1500
-    detector = cv2.SimpleBlobDetector_create(detector_params)
+	detector_params = cv2.SimpleBlobDetector_Params()
+	detector_params.filterByArea = True
+	detector_params.maxArea = 1500
+	detector = cv2.SimpleBlobDetector_create(detector_params)
