4ccd9f04e1423a50310afc84912bde0d592ccb22
yannanz913 <69208032+yannanz913@users.noreply.github.com>
2020-12-16 17:57:45 +0800

    Update rat eye detection.py

--- rat eye detection.py
+++ rat eye detection.py
@@ -15,7 +15,7 @@ for i in circles[0,:]:
     cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
     cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected the pupil',img)
+cv2.imshow('detected pupil',img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
