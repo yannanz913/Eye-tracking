5604d2213b43424093f0394670058c6d2996878c
yannanz913 <69208032+yannanz913@users.noreply.github.com>
2021-01-20 14:49:59 +0800

    Update detector.py

--- detector.py
+++ detector.py
@@ -2,26 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-def detect_pupil(img, pupil_detector):
+def build_pupil_detector(circles):
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,50,
+                                param1=50,param2=50,minRadius=0,maxRadius=0)
+
+def pupil_detector(img, pupil_detector):
     img = cv2.imread("/Users/macair/Desktop/VH lab/rat eye.png")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cimg = img.copy()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.medianBlur(img, 5)
-    pupil_detector = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,50,
-                                param1=50,param2=50,minRadius=0,maxRadius=0)
+    pupil_detector = circles
 
-    pupil_detector = np.uint16(np.around(circles))
+    circles = np.uint16(np.around(circles))
     for i in circles[0,:]:
         cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
         cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
     return img
 
 
-def detect_glints(img, glints_detector):
-    img = cv2.imread("/Users/macair/Desktop/VH lab/rat eye2.png", cv2.IMREAD_GRAYSCALE)
-    retval, threshold = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
-
+def build_glints_detector(params):
     params = cv2.SimpleBlobDetector_Params()
 
     params.minThreshold = 10
@@ -41,6 +41,10 @@ def detect_glints(img, glints_detector):
     params.filterByInertia = True
     params.minInertiaRatio = 0.001
 
+def glints_detector(img, glints_detector):
+    img = cv2.imread("/Users/macair/Desktop/VH lab/rat eye2.png", cv2.IMREAD_GRAYSCALE)
+    retval, threshold = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY_INV)
+    
     ver = (cv2.__version__).split('.')
     if int(ver[0]) < 3:
         glints_detector = cv2.SimpleBlobDetector(params)
@@ -52,6 +56,7 @@ def detect_glints(img, glints_detector):
 
     img_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     return img_with_keypoints
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -71,3 +76,4 @@ def main():
 
 def nothing(x):
     pass
+
