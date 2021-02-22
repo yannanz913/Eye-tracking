e565480f9ca2c4dcbfd833ee3519dd5dfefbd6c3
Steve Van Hooser <vanhoosr@brandeis.edu>
2021-02-22 08:24:55 -0500

    edits to read EyeTrackerParameters

--- EyeTrackerParameterWindow2.py
+++ EyeTrackerParameterWindow2.py
@@ -21,30 +21,29 @@ class EyeTrackerParameterWindow(QWidget):
         maxRadius = QLabel('PupilmaxRadius')
         Threshold = QLabel('PupilThreshold')
 
-        param1Edit = QLineEdit()
-        param2Edit = QLineEdit()
-        minRadiusEdit = QLineEdit()
-        maxRadiusEdit = QLineEdit()
-        ThresholdEdit = QLineEdit()
-        editFields = [param1Edit, param2Edit, minRadiusEdit, maxRadiusEdit, ThresholdEdit]
+        self.param1Edit = QLineEdit()
+        self.param2Edit = QLineEdit()
+        self.minRadiusEdit = QLineEdit()
+        self.maxRadiusEdit = QLineEdit()
+        self.ThresholdEdit = QLineEdit()
         
         grid = QGridLayout()
         grid.setSpacing(10)
 
         grid.addWidget(param1, 1, 0)
-        grid.addWidget(param1Edit, 1, 1)
+        grid.addWidget(self.param1Edit, 1, 1)
 
         grid.addWidget(param2, 2, 0)
-        grid.addWidget(param2Edit, 2, 1)
+        grid.addWidget(self.param2Edit, 2, 1)
 
         grid.addWidget(minRadius, 3, 0)
-        grid.addWidget(minRadiusEdit, 3, 1)
+        grid.addWidget(self.minRadiusEdit, 3, 1)
 
         grid.addWidget(maxRadius, 4, 0)
-        grid.addWidget(maxRadiusEdit, 4, 1)
+        grid.addWidget(self.maxRadiusEdit, 4, 1)
 
         grid.addWidget(Threshold, 5, 0)
-        grid.addWidget(ThresholdEdit, 5, 1)
+        grid.addWidget(self.ThresholdEdit, 5, 1)
 
         self.pybutton = QPushButton('OK', self)
         grid.addWidget(self.pybutton, 6, 1)
@@ -56,13 +55,30 @@ class EyeTrackerParameterWindow(QWidget):
         self.setWindowTitle('Enter Parameters For Detecting Pupil')    
 
     def getint(self):
-        param1, done1 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'Pupilparam1')
-        param2, done2 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'Pupilparam2')
-        minRadius, done3 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilminRadius')
-        maxRadius, done4 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilmaxRadius')
-        Threshold, done5 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilThreshold')
-        if done1 and done2 and done3 and done4 and done5 : 
+        pupilParams = {
+           "PupilParam1": 0,
+           "PupilParam2": 0,
+           "minRadiusEdit": 0,
+           "maxRadiusEdit": 0,
+           "Threshold": 0
+        }
+        glintParams = {
+           "Glint1": 0,
+           "Glint2": 0
+        }
+
+        threshold_string = self.ThresholdEdit.text();
+        pupilParams["Threshold"] = int(threshold_string)
+        print(pupilParams)
+        #param1, done1 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'Pupilparam1')
+        #param2, done2 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'Pupilparam2')
+        #minRadius, done3 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilminRadius')
+        #maxRadius, done4 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilmaxRadius')
+        #Threshold, done5 = QtWidgets.QInputDialog.getInt(self, 'Enter Parameters For Detecting Pupil', 'PupilThreshold')
+        #if done1 and done2 and done3 and done4 and done5 : 
             # set parameters to be those in pupil detection function
+        #    a=1
+        return pupilParams;
 
             
 if __name__ == "__main__":
@@ -70,3 +86,4 @@ if __name__ == "__main__":
     mainWin = EyeTrackerParameterWindow()
     mainWin.show()
     sys.exit( app.exec_() )
+
