9e1ca44ae207470042ceb8ab6eb570ceb5136fdc
yannanz913 <69208032+yannanz913@users.noreply.github.com>
2021-04-07 07:56:25 -0400

    Update EyeTrackerImageWindow.py

--- EyeTrackerImageWindow.py
+++ EyeTrackerImageWindow.py
@@ -29,3 +29,27 @@ class EyeTrackerImageWindow(QDialog):
 App = QApplication(sys.argv)
 window = EyeTrackerImageWindow()
 sys.exit(App.exec())
+
+class EyeDetector:
+    def build_pupil_detector(circles):
+        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,50,
+                                    param1=pupilParams['Pupil_Param1'], param2=pupilParams['Pupil_Param2'], minRadius=pupilParams['Pupil_minRadius'], maxRadius=pupilParams['Pupil_maxRadius'])
+
+    def pupil_detector(img, pupil_detector):
+        img = cv2.imread("/Users/macair/Desktop/VH lab/rat eye.png")
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cimg = img.copy()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.medianBlur(img, 5)
+        pupil_detector = circles
+
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+        return img
+
+    def Main():
+        left_pupil_tracking = EyeTrackerImageWindow(QDialog)
+        pupil_detecting = EyeDetector()
+        left_pupil_tracking.InitWindow(self,pupil_detecting)
