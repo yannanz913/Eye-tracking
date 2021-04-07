e3159c1d4a35a1c97e326d31837d6aa146fd4348
yannanz913 <69208032+yannanz913@users.noreply.github.com>
2021-04-07 06:50:48 -0400

    Update EyeTrackerImageWindow.py

--- EyeTrackerImageWindow.py
+++ EyeTrackerImageWindow.py
@@ -1,7 +1,31 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel
+import sys
+from PyQt5.QtGui import QPixmap
 
+class EyeTrackerImageWindow(QDialog):
+  
+    def __init__(self):
+        super().__init__()
+        self.title = "left eye tracking"
+        self.top = 200
+        self.left = 500
+        self.width = 400
+        self.height = 300
+        self.InitWindow()
 
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("/Users/elainezhu/Desktop/VH lab/rat eye.png")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.setLayout(vbox)
+        self.show()
 
-
-class EyeTrackerImageWindow:
-
-
+    
+App = QApplication(sys.argv)
+window = EyeTrackerImageWindow()
+sys.exit(App.exec())
